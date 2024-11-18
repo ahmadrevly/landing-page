89e1426514724013c32f9607914466aa386912c7
Revly <ahmadrevlyyy@gmail.com>
2024-11-18 13:44:56 +0700

    landing page

--- js/script.js
+++ js/script.js
@@ -18,6 +18,17 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// Klik donasi sekarang langsung ke DM instagram
+
+document
+  .getElementById("donasiNow")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const url = `https://www.instagram.com/direct/t/106102004124694`;
+    window.open(url, "_blank");
+  });
+
 // Klik kirim pesan langsung ke DM instagram
 
 document
@@ -31,6 +42,6 @@ document
     // Membuat pesan dengan template
     const pesan = `Halo, nama saya ${nama} dan username instagram saya ${instagram}. Saya mau mendonasikan beberapa buku, apakah bisa?`;
     const encodedPesan = encodeURIComponent(pesan);
-    const url = `https://www.instagram.com/direct/t/106102004124694/`;
+    const url = `https://www.instagram.com/direct/t/106102004124694`;
     window.open(url, "_blank");
   });
